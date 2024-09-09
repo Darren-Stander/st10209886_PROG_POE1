4a85b59ff5caf0f6421e63ffe8c8498382a53162
Darren-Stander <darrenjadestander@gmail.com>
2024-09-09 16:45:09 +0200

    Add project files.

---
+++ Controllers/ClaimController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace st10209886_PROG_POE1.Controllers
+{
+    public class ClaimController : Controller
+    {
+        // GET: Claims/Submit
+        public IActionResult Submit()
+        {
+            return View();
+        }
+
+        // GET: Claims/History
+        public IActionResult History()
+        {
+            return View();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+namespace st10209886_PROG_POE1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            // This sets up the default route for MVC
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
